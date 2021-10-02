367a1da489e26aaf2b2f45084bd856e698fe2776
chhatrapalsinhzala7 <chhatrapalsinhzala7@gmail.com>
2021-10-02 15:54:25 +0530

    simple calc
    I have created simple calc using java.
    
    Mam  i am contributing this under hactoberfest 2021 , so please assign me a label of hactoberfest2021.

---
+++ AppDev/Java/Simple_calculator/app/src/main/java/com/example/final_calculator/MainActivity.java
@@ -0,0 +1,225 @@
+package com.example.final_calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.ArrayList;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    TextView workingsTV;
+    TextView resultsTV;
+
+    String workings = "";
+    String formula = "";
+    String tempFormula = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
+        getSupportActionBar().hide(); //hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //show the activity in full screen
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initTextViews();
+    }
+
+    private void initTextViews()
+    {
+        workingsTV = (TextView)findViewById(R.id.workingsTextView);
+        resultsTV = (TextView)findViewById(R.id.resultTextView);
+    }
+
+    private void setWorkings(String givenValue)
+    {
+        workings = workings + givenValue;
+        workingsTV.setText(workings);
+    }
+
+
+    public void equalsOnClick(View view)
+    {
+        Double result = null;
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("rhino");
+        checkForPowerOf();
+
+        try {
+            result = (double)engine.eval(formula);
+        } catch (ScriptException e)
+        {
+            Toast.makeText(this, "Invalid Input", Toast.LENGTH_SHORT).show();
+        }
+
+        if(result != null)
+            resultsTV.setText(String.valueOf(result.doubleValue()));
+
+    }
+
+    private void checkForPowerOf()
+    {
+        ArrayList<Integer> indexOfPowers = new ArrayList<>();
+        for(int i = 0; i < workings.length(); i++)
+        {
+            if (workings.charAt(i) == '^')
+                indexOfPowers.add(i);
+        }
+
+        formula = workings;
+        tempFormula = workings;
+        for(Integer index: indexOfPowers)
+        {
+            changeFormula(index);
+        }
+        formula = tempFormula;
+    }
+
+    private void changeFormula(Integer index)
+    {
+        String numberLeft = "";
+        String numberRight = "";
+
+        for(int i = index + 1; i< workings.length(); i++)
+        {
+            if(isNumeric(workings.charAt(i)))
+                numberRight = numberRight + workings.charAt(i);
+            else
+                break;
+        }
+
+        for(int i = index - 1; i >= 0; i--)
+        {
+            if(isNumeric(workings.charAt(i)))
+                numberLeft = numberLeft + workings.charAt(i);
+            else
+                break;
+        }
+
+        String original = numberLeft + "^" + numberRight;
+        String changed = "Math.pow("+numberLeft+","+numberRight+")";
+        tempFormula = tempFormula.replace(original,changed);
+    }
+
+    private boolean isNumeric(char c)
+    {
+        if((c <= '9' && c >= '0') || c == '.')
+            return true;
+
+        return false;
+    }
+
+
+    public void clearOnClick(View view)
+    {
+        workingsTV.setText("");
+        workings = "";
+        resultsTV.setText("");
+        leftBracket = true;
+    }
+
+    boolean leftBracket = true;
+
+    public void bracketsOnClick(View view)
+    {
+        if(leftBracket)
+        {
+            setWorkings("(");
+            leftBracket = false;
+        }
+        else
+        {
+            setWorkings(")");
+            leftBracket = true;
+        }
+    }
+
+    public void powerOfOnClick(View view)
+    {
+        setWorkings("^");
+    }
+
+    public void divisionOnClick(View view)
+    {
+        setWorkings("/");
+    }
+
+    public void sevenOnClick(View view)
+    {
+        setWorkings("7");
+    }
+
+    public void eightOnClick(View view)
+    {
+        setWorkings("8");
+    }
+
+    public void nineOnClick(View view)
+    {
+        setWorkings("9");
+    }
+
+    public void timesOnClick(View view)
+    {
+        setWorkings("*");
+    }
+
+    public void fourOnClick(View view)
+    {
+        setWorkings("4");
+    }
+
+    public void fiveOnClick(View view)
+    {
+        setWorkings("5");
+    }
+
+    public void sixOnClick(View view)
+    {
+        setWorkings("6");
+    }
+
+    public void minusOnClick(View view)
+    {
+        setWorkings("-");
+    }
+
+    public void oneOnClick(View view)
+    {
+        setWorkings("1");
+    }
+
+    public void twoOnClick(View view)
+    {
+        setWorkings("2");
+    }
+
+    public void threeOnClick(View view)
+    {
+        setWorkings("3");
+    }
+
+    public void plusOnClick(View view)
+    {
+        setWorkings("+");
+    }
+
+    public void decimalOnClick(View view)
+    {
+        setWorkings(".");
+    }
+
+    public void zeroOnClick(View view)
+    {
+        setWorkings("0");
+    }
+
+}

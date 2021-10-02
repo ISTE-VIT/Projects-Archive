448033d71817ea4ac54b0955713bda82515646a3
Vyom Gupta <vyom.gupta2020@vitstudent.ac.in>
2021-10-02 19:09:56 +0530

    Added some more features

--- Machine Learning/01 Automation & Bots/Send Automated Messages/main.py
+++ Machine Learning/01 Automation & Bots/Send Automated Messages/main.py
@@ -1,2 +1,7 @@
 import pywhatkit
-pywhatkit.sendwhatmsg("+919999999999", "ISTE present you to an insightful webinar", 10, 30)
+#send message to even unknwon number from whatsapp
+pywhatkit.sendwhatmsg("+918077571109", "ISTE present you to an insightful webinar", 19, 2)
+#will play video of the given channel on youtube. You can even add the link here of the video
+pywhatkit.playonyt('ISTE-VIT')
+#will search for the given text.
+pywhatkit.search('hacktoberfest')

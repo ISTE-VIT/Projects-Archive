503ba11bf97432e384bbd518052c9d8b0fbccf5e
sagefell29 <77113275+sagefell29@users.noreply.github.com>
2021-10-05 12:50:01 +0530

    Added Meme-App in Java
    App Development/Meme-App/Java

---
+++ AppDev/Java/Meme-App/app/src/main/java/com/alok/memeshare/SplashScreen.java
@@ -0,0 +1,20 @@
+package com.alok.memeshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashScreen.this, MainActivity.class));
+            finish();
+        },2100);
+    }
+}

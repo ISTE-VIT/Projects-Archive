63a203ce199a960de86902b25f690b6cb1c7b63f
Manu Vairagi <75036590+ManuVairagi1@users.noreply.github.com>
2021-10-07 19:43:15 +0530

    Updated UI and  added firebase auth

--- WebDev/3-Advanced/Netflix_Clone/netflix_clone/src/Components/Rows/Rows.js
+++ WebDev/3-Advanced/Netflix_Clone/netflix_clone/src/Components/Rows/Rows.js
@@ -17,6 +17,10 @@ function Row({ title, fetchURL }) {
     fetchData();
   }, [fetchURL]);
 
+  function truncate(string, n) {
+    return string?.length > n ? string.substr(0, n - 1) + "..." : string;
+  }
+
   console.log(movies);
 
   return (
@@ -24,13 +28,23 @@ function Row({ title, fetchURL }) {
       <h2>{title}</h2>
       <div className="row_posters">
         {movies.map((movie) => (
-          <img
-            className="row_poster"
-            key={movie.id}
-            src={`${base_url}${movie.backdrop_path}`}
-            alt={movie.name}
-          />
+          <div className="row__posters">
+            <img
+              className="row_poster"
+              key={movie.id}
+              src={`${base_url}${movie.backdrop_path}`}
+              alt={movie.name}
+            />
+            <div className="movie__name">
+              {/* {movie?.name || movie?.title || movie?.original_name} */}
+              {truncate(
+                movie?.name || movie?.title || movie?.original_name,
+                15
+              )}
+            </div>
+          </div>
         ))}
+        {/* {movies.map((movie) => ( */}
       </div>
     </div>
   );
